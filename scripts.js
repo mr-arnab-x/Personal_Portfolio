c70d3519c53f3752f46f556fa3d84ce13f06e763
mr-arnab-x <santraarnab241@gmail.com>
2024-09-03 13:05:31 +0530

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// js/scripts.js
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+    // Here you can add code to process the form data
+});
